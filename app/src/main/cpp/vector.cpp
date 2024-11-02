d3959ebe0330df987bfee4aae1ed63feec4f58d8
GoodDamn <grigorydum@gmail.com>
2024-11-02 14:38:40 +0300

    VEUtilsIntJava

---
+++ app/src/main/cpp/vector.cpp
@@ -0,0 +1,18 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("vector");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("vector")
+//      }
+//    }
+
